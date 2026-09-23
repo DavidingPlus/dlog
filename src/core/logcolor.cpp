9f7d9e37a7b83f38615a331ae588ac337b0c5abc
DavidingPlus <davidingplus@qq.com>
2026-09-23 20:03:13 +0800

    rename prefix to DLOG_COLOR

--- src/core/logcolor.cpp
+++ src/core/logcolor.cpp
@@ -8,22 +8,22 @@ LogColorGuard::LogColorGuard(LogLevel level)
     switch (level)
     {
         case LogLevel::TRACE:
-            std::cout << LOG_COLOR_TRACE;
+            std::cout << DLOG_COLOR_TRACE;
             break;
         case LogLevel::DEBUG:
-            std::cout << LOG_COLOR_DEBUG;
+            std::cout << DLOG_COLOR_DEBUG;
             break;
         case LogLevel::INFO:
-            std::cout << LOG_COLOR_INFO;
+            std::cout << DLOG_COLOR_INFO;
             break;
         case LogLevel::WARN:
-            std::cout << LOG_COLOR_WARN;
+            std::cout << DLOG_COLOR_WARN;
             break;
         case LogLevel::ERROR:
-            std::cout << LOG_COLOR_ERROR;
+            std::cout << DLOG_COLOR_ERROR;
             break;
         case LogLevel::FATAL:
-            std::cout << LOG_COLOR_FATAL;
+            std::cout << DLOG_COLOR_FATAL;
             break;
     }
 }
--- src/core/logcolor.h
+++ src/core/logcolor.h
@@ -9,25 +9,25 @@ enum class LogLevel;
 
 
 // 清除所有颜色，恢复到默认。
-#define LOG_COLOR_RESET "\033[0m"
+#define DLOG_COLOR_RESET "\033[0m"
 
 // 跟踪信息，白色。
-#define LOG_COLOR_TRACE "\033[37m"
+#define DLOG_COLOR_TRACE "\033[37m"
 
 // 调试信息，青色。
-#define LOG_COLOR_DEBUG "\033[36m"
+#define DLOG_COLOR_DEBUG "\033[36m"
 
 // 普通信息，绿色。
-#define LOG_COLOR_INFO "\033[32m"
+#define DLOG_COLOR_INFO "\033[32m"
 
 // 警告信息，粗体黄色。
-#define LOG_COLOR_WARN "\033[33m\033[1m"
+#define DLOG_COLOR_WARN "\033[33m\033[1m"
 
 // 错误信息，粗体红色。
-#define LOG_COLOR_ERROR "\033[31m\033[1m"
+#define DLOG_COLOR_ERROR "\033[31m\033[1m"
 
 // 致命错误，粗体红底。
-#define LOG_COLOR_FATAL "\033[1m\033[41m"
+#define DLOG_COLOR_FATAL "\033[1m\033[41m"
 
 
 class D_API_EXPORTED LogColorGuard
@@ -39,7 +39,7 @@ public:
     explicit LogColorGuard(LogLevel level);
 
     // 析构时自动恢复原始颜色。
-    ~LogColorGuard() { std::cout << LOG_COLOR_RESET << std::flush; }
+    ~LogColorGuard() { std::cout << DLOG_COLOR_RESET << std::flush; }
 };
 
 
